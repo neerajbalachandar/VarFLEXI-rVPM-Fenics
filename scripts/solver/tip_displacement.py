d520df2004854144dad491a59db81c3249508962
Padmaprabhan2004 <padmaprabhan2004@gmail.com>
2026-07-20 21:54:17 +0530

    tip displacement comments

--- scripts/solver/tip_displacement.py
+++ scripts/solver/tip_displacement.py
@@ -1,3 +1,4 @@
-# TODO : tip displacement (m) vs time for dt = 0.002, 0.001, 0.0005 seconds
+# tip displacement (m) vs time for dt = 0.002, 0.001, 0.0005 seconds
+# strain energy (joules) vs time for dt = 0.002, 0.001, 0.0005 seconds
 
-# TODO: strain energy (joules) vs time for dt = 0.002, 0.001, 0.0005 seconds
+#isnt this same as dt.py? so can be inferred from there.
